e8c74d6a8ed2f885a639ef2705236420239daad9
Sara-331 <saraa_130_@hotmail.com>
2024-11-25 16:17:34 +0300

    sara
    .

--- Project1/obj/Debug/net9.0/Project1.AssemblyInfo.cs
+++ Project1/obj/Debug/net9.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efcee6e9cbf03e2d0c871173782081ea1d990b6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
